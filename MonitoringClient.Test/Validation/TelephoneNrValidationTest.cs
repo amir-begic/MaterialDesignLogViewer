6708c3261714aa0e59c7b4c3b390f1545851efe9
Amir Begic <amir.begic@namics.com>
2019-08-11 22:55:31 +0200

    Task5: Added TelephoneNrValidation and added tests.

---
+++ MonitoringClient.Test/Validation/TelephoneNrValidationTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MonitoringClient.Validation;
+
+namespace MonitoringClient.Test.Validation
+{
+    [TestClass]
+    public class TelephoneNrValidationTest
+    {
+        [TestMethod]
+        public void Is_Swiss_International_Number()
+        {
+            var phoneNrValidation = new TelephoneNrValidation();
+
+            var phoneNr1 = "+41 75 409 00 00";
+            var phoneNr2 = "0041 75 409 00 00";
+            var phoneNr3 = "0041 (0)75 409 00 00";
+
+            var result1 = phoneNrValidation.Validate(phoneNr1, CultureInfo.CurrentCulture);
+            var result2 = phoneNrValidation.Validate(phoneNr2, CultureInfo.CurrentCulture);
+            var result3 = phoneNrValidation.Validate(phoneNr3, CultureInfo.CurrentCulture);
+
+            Assert.IsTrue(result1.IsValid && result2.IsValid && result3.IsValid);
+        }
+
+        [TestMethod]
+        public void Is_German_International_Number()
+        {
+            var phoneNrValidation = new TelephoneNrValidation();
+
+            var phoneNr1 = "+49 75 409 00 00";
+            var phoneNr2 = "0049 (0)69 365 9198";
+            var phoneNr3 = "+49 (0)69 365 9198";
+
+            var result1 = phoneNrValidation.Validate(phoneNr1, CultureInfo.CurrentCulture);
+            var result2 = phoneNrValidation.Validate(phoneNr2, CultureInfo.CurrentCulture);
+            var result3 = phoneNrValidation.Validate(phoneNr3, CultureInfo.CurrentCulture);
+
+            Assert.IsTrue(result1.IsValid && result2.IsValid && result3.IsValid);
+        }
+
+        [TestMethod]
+        public void Is_Lichtenstein_International_Number()
+        {
+            var phoneNrValidation = new TelephoneNrValidation();
+
+            var phoneNr1 = "+423 75 409 00 00";
+            var phoneNr2 = "00423 75 409 00 00";
+            var phoneNr3 = "00423 (0)75 409 00 00";
+
+            var result1 = phoneNrValidation.Validate(phoneNr1, CultureInfo.CurrentCulture);
+            var result2 = phoneNrValidation.Validate(phoneNr2, CultureInfo.CurrentCulture);
+
+            Assert.IsTrue(result1.IsValid && result2.IsValid);
+        }
+
+        [TestMethod]
+        public void Is_Local_All_Countries()
+        {
+            var phoneNrValidation = new TelephoneNrValidation();
+
+            var phoneNr1 = "069 365 9198";
+            var phoneNr2 = "075 409 00-00";
+            var phoneNr3 = "023 / 214 54";
+
+            var result1 = phoneNrValidation.Validate(phoneNr1, CultureInfo.CurrentCulture);
+            var result2 = phoneNrValidation.Validate(phoneNr2, CultureInfo.CurrentCulture);
+            var result3 = phoneNrValidation.Validate(phoneNr3, CultureInfo.CurrentCulture);
+
+            Assert.IsTrue(result1.IsValid && result2.IsValid && result3.IsValid);
+        }
+    }
+}
--- MonitoringClient/Validation/TelephoneNrValidation.cs
+++ MonitoringClient/Validation/TelephoneNrValidation.cs
@@ -11,22 +11,42 @@ namespace MonitoringClient.Validation
 {
     public class TelephoneNrValidation : ValidationRule
     {
-        private string _patternAreaCodeCH;
-        private string _patternAreaCodeDE;
-        private string _patternAreaCodeLI;
-        private Regex _regex;
+        private readonly string _patternLocal = "^([0-9()]{3})\\/?([0-9]{5})-?([0-9]{2})?$";
+        private readonly string _patternAreaCodeInternationalCh = "^(\\+41|0041)(?:\\(0\\))?([0-9()]{2})([0-9]{5})-?([0-9]{2})?$";
+        private readonly string _patternAreaCodeInternationalDe = "^(\\+49|0049)(?:\\(0\\))?([0-9()]{2})([0-9]{5})-?([0-9]{2})?$";
+        private readonly string _patternAreaCodeInternationalLi = "^(\\+423|00423)(?:\\(0\\))?([0-9()]{2})([0-9]{5})-?([0-9]{2})?$";
+
+        private readonly Regex _regexLocal;
+        private readonly Regex _regexAreaCodeCHInternational;
+        private readonly Regex _regexAreaCodeDEInternational;
+        private readonly Regex _regexAreaCodeLInternationalI;
+
 
         public TelephoneNrValidation()
         {
-            _pattern = "^.*";
-            _regex = new Regex(_pattern);
+            //_patternAreaCodeCH = "";
+            _regexLocal = new Regex(_patternLocal);
+            _regexAreaCodeCHInternational = new Regex(_patternAreaCodeInternationalCh);
+            _regexAreaCodeDEInternational = new Regex(_patternAreaCodeInternationalDe);
+            _regexAreaCodeLInternationalI = new Regex(_patternAreaCodeInternationalLi);
+
         }
 
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
             var tel = value as string;
+            if (tel == null)
+            {
+                return new ValidationResult(false, "value was null.");
+            }
 
-            if (value == null || !_regex.Match(tel).Success)
+            tel = tel.Replace(" ", string.Empty);
+            
+
+            if (tel == null || !_regexAreaCodeCHInternational.Match(tel).Success
+                            && !_regexAreaCodeDEInternational.Match(tel).Success
+                            && !_regexAreaCodeLInternationalI.Match(tel).Success
+                            && !_regexLocal.Match(tel).Success)
             {
                 return new ValidationResult(false, "The value is not a valid phone number!");
             }
